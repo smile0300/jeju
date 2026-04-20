7236747e93918046ed0a3e463c837b0ebc134bbf
SMILE\k9746 <k97460300@gmail.com>
2026-04-21 00:17:36 +0900

    style: adjust 'Date' label position in legend

--- src/js/weather.js
+++ src/js/weather.js
@@ -579,7 +579,7 @@ export function updateHourlyWeather(locKey) {
     <div class="hourly-wrapper">
         <div class="hourly-legend">
             <div class="h-top-section h-legend-top" style="position: relative;">
-                <span class="h-date" style="position: absolute; top: 7px; left: 50%; transform: translateX(-50%); font-size:0.65rem; font-weight:800; color:#adb5bd; white-space: nowrap;">日期</span>
+                <span class="h-date" style="position: absolute; top: -4px; left: 50%; transform: translateX(-50%); font-size:0.65rem; font-weight:800; color:#adb5bd; white-space: nowrap;">日期</span>
                 <span class="h-time" style="font-size:0.65rem; font-weight:800; color:#adb5bd;">时间</span>
                 <span class="h-icon" style="visibility:hidden;">-</span>
                 <span class="h-pop" style="visibility:hidden; margin-top: 5px;">-</span>
