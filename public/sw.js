ca0b7c7bab3dfe0fe4515f8af1a14e73e30c1773
SMILE\k9746 <k97460300@gmail.com>
2026-08-09 23:16:10 +0900

    fix: revert firebase to 12.17.0

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
 const firebaseConfig = {
     apiKey: "AIzaSyARSW6PIIqjICd3IZwbgneNQ-4xZfEsJsc",
